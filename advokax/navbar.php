6c04c9bc8ad138c36bc478663c8be4fdc8e25b16
einsvein <36289813+einsvein@users.noreply.github.com>
2025-02-19 12:11:07 +0100

    Create navbar.php

---
+++ advokax/navbar.php
@@ -0,0 +1,34 @@
+<!-- navbar.php -->
+<nav>
+    <ul>
+        <li><a href="register.php">Register</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="submit_info.php">Submit Info</a></li>
+        <li><a href="edit_info.php">Edit Info</a></li>
+    </ul>
+</nav>
+
+<style>
+    nav {
+        background-color: #333;
+        overflow: hidden;
+    }
+    nav ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+    }
+    nav ul li {
+        float: left;
+    }
+    nav ul li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+    nav ul li a:hover {
+       ;
+    }
+</style>
